0727aca358d489b97d8a08c1f29245f9d7bd6164
rungene <rungenesamuel@gmail.com>
2023-03-30 02:48:54 -0700

    Corrections

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,21 +14,18 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (parent == NULL || value == '0')
+	if (parent == NULL || value == '\0')
 		return (NULL);
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 	/*insert left */
 	/*if data to be inserted is lesser, insert in left subtree.*/
-	if (value >  parent->n)
+	if (parent->right != NULL)
 	{
-		if (parent->right != NULL)
-		{
-			new_node->right = parent->right;
-			new_node->right->parent = new_node;
-		}
-		parent->right = new_node;
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
 	}
+	parent->right = new_node;
 	return (new_node);
 }
