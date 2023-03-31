0075fa747aed04a662eb1e2505565fda6361ea9a
rungene <rungenesamuel@gmail.com>
2023-03-30 19:39:21 -0700

    Corrections

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,14 +10,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	/*tree is NULL*/
 	if (tree == NULL)
 		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	if (tree->left)
+		left_height += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height += 1 + binary_tree_height(tree->right);
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
