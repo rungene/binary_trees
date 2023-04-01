e17b61c050c0050a1e9c135bb42848d77dafed6b
rungene <rungenesamuel@gmail.com>
2023-03-31 19:55:28 -0700

    Refactor: Removing unnecessary brackets

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,9 +16,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 	/*If both left and right are not NULL, and left &*/
 	/* right subtrees are full*/
-	if ((tree->left) && (tree->right))
-		return ((binary_tree_is_full(tree->left))
-				&& (binary_tree_is_full(tree->right)));
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left)
+				&& binary_tree_is_full(tree->right));
 	/*We reach here when none of the above if conditions work*/
 	return (0);
 }
