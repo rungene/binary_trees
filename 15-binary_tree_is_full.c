83bb2b2dbe34b14d8eea1ed3709ab406f1a3fd45
rungene <rungenesamuel@gmail.com>
2023-03-31 19:58:16 -0700

    ref

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	/*If empty tree*/
 	if (tree == NULL)
-		return (1);
+		return (0);
 	/*If leaf node*/
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
